689535e6e5d6712548f2ffdce8521ff1a7b1d0ba
Pierre Wieser <p.wieser@trychlos.org>
2024-10-23 13:38:37 +0200

    ecmascript is used on both sides

--- package.js
+++ package.js
@@ -25,7 +25,7 @@ Package.onTest( function( api ){
 function configure( api ){
     api.versionsFrom([ '2.9.0', '3.0-rc.0' ]);
     api.use( 'blaze-html-templates@2.0.0 || 3.0.0-alpha300.0', 'client' );
-    api.use( 'ecmascript', 'client' );
+    api.use( 'ecmascript' );
     api.use( 'less@4.0.0', 'client' );
     api.use( 'pwix:i18n@1.5.0' );
     api.use( 'pwix:ui-bootstrap5@2.0.0', 'client' );
